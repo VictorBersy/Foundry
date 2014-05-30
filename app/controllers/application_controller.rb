2169d9c545eb23486155eba1c996f08d133efcb8
Victor Bersy <victorbersy+github@gmail.com>
2014-05-31 00:50:07 +0200

    add basic internationalization system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,34 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
   def set_locale
-    I18n.locale = extract_locale_from_subdomain || I18n.default_locale
+    I18n.locale = retrieve_locale || I18n.default_locale
   end
 
   protected
 
-  def extract_locale_from_subdomain
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
+
+  def retrieve_locale
+    locale_from_cookie || locale_from_subdomain || locale_from_header
+  end
+
+  def locale_from_cookie
+    cookies[:language]
+  end
+
+  def locale_from_subdomain
     parsed_locale = request.subdomains.first
     return nil if parsed_locale.nil?
     I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale : nil
+  end
+
+  def locale_from_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 end
