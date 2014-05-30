38597ca6fe41a3f2c724556d10b6eb995b3f3e08
Victor Bersy <victorbersy+github@gmail.com>
2014-05-31 00:49:45 +0200

    fix issue username and screen_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :set_attributes
+
+  private
+
+  def set_attributes
+    self.screen_name = username
+    self.username    = username.downcase
+  end
 end
